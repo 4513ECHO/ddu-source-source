7cebf75f4003c7282e02e3f2be485149e7f3bad9
4513ECHO <4513echo@gmail.com>
2022-05-04 18:08:17 +0900

    fix: Correct action params

--- denops/@ddu-kinds/source.ts
+++ denops/@ddu-kinds/source.ts
@@ -11,20 +11,22 @@ export interface ActionData {
   name: string;
 }
 type Params = Record<never, never>;
+type ActionParams = Record<string, unknown>;
 
 export class Kind extends BaseKind<Params> {
   actions: Actions<Params> = {
     async execute(args) {
+      const actionParams = args.actionParams as ActionParams;
       const options: DduOptions = {
         sources: args.items.map((i) => {
           const action = i?.action as ActionData;
           return {
             name: action.name,
+            params: actionParams,
           };
         }),
       };
-      const actionParams = args.actionParams as Pick<DduOptions, "sources">;
-      await args.denops.call("ddu#start", { ...options, ...actionParams });
+      await args.denops.call("ddu#start", options);
       return Promise.resolve(ActionFlags.None);
     },
   };
